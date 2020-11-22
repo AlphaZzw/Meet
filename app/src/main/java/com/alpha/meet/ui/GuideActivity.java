ef082ae96dd7cb7d769897376e0e84d26404a851
zhuzhengwei <zzw13655533995@163.com>
2020-11-22 21:54:15 +0800

    BasePagerAdapter ViewPager适配器

--- app/src/main/java/com/alpha/meet/ui/GuideActivity.java
+++ app/src/main/java/com/alpha/meet/ui/GuideActivity.java
@@ -8,11 +8,18 @@ import com.alpha.meet.R;
 
 public class GuideActivity extends BaseUIActivity {
 
+
+    /**
+     * 1.ViewPager : 适配器|帧动画播放
+     * 2.小圆点的逻辑
+     * 3.歌曲的播放
+     * 4.属性动画旋转
+     * 5.跳转
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_guide);
-        LogUtils.i("Guide");
     }
 
 }
---
+++ framework/src/main/java/com/alpha/framework/base/BasePagerAdapter.java
@@ -0,0 +1,40 @@
+package com.alpha.framework.base;
+
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.viewpager.widget.PagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
+import java.util.List;
+
+public class BasePagerAdapter extends PagerAdapter {
+
+    private List<View> mList;
+
+    public BasePagerAdapter(List<View> list) {
+        mList = list;
+    }
+
+    @Override
+    public int getCount() {
+        return mList.size();
+    }
+
+    @Override
+    public boolean isViewFromObject(View view, Object object) {
+        return view == object;
+    }
+
+    @Override
+    public Object instantiateItem(ViewGroup container, int position) {
+        ((ViewPager) container).addView(mList.get(position));
+        return mList.get(position);
+    }
+
+    @Override
+    public void destroyItem(ViewGroup container, int position, Object object) {
+        ((ViewPager) container).removeView(mList.get(position));
+
+    }
+}
