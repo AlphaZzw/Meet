f970f1f94fb77d2b859537cf8238f2db483530a2
zhuzhengwei <zzw13655533995@163.com>
2020-11-22 22:08:11 +0800

    引导页ViewPager Layout

--- app/src/main/java/com/alpha/meet/ui/GuideActivity.java
+++ app/src/main/java/com/alpha/meet/ui/GuideActivity.java
@@ -1,6 +1,11 @@
 package com.alpha.meet.ui;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.viewpager.widget.ViewPager;
 
 import com.alpha.framework.base.BaseUIActivity;
 import com.alpha.framework.utils.LogUtils;
@@ -8,6 +13,12 @@ import com.alpha.meet.R;
 
 public class GuideActivity extends BaseUIActivity {
 
+    private ImageView iv_music_switch;
+    private TextView tv_guide_skip;
+    private ImageView iv_guide_point_1;
+    private ImageView iv_guide_point_2;
+    private ImageView iv_guide_point_3;
+    private ViewPager mViewPager;
 
     /**
      * 1.ViewPager : 适配器|帧动画播放
@@ -20,6 +31,18 @@ public class GuideActivity extends BaseUIActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_guide);
+        initView();
+    }
+
+    private void initView() {
+        iv_music_switch = (ImageView) findViewById(R.id.iv_music_switch);
+        tv_guide_skip = (TextView) findViewById(R.id.tv_guide_skip);
+        iv_guide_point_1 = (ImageView) findViewById(R.id.iv_guide_point_1);
+        iv_guide_point_2 = (ImageView) findViewById(R.id.iv_guide_point_2);
+        iv_guide_point_3 = (ImageView) findViewById(R.id.iv_guide_point_3);
+        mViewPager = (ViewPager) findViewById(R.id.mViewPager);
+
+        View.inflate(this, R.layout.layout_pager_guide_1, null);
     }
 
 }
