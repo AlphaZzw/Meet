dea2bc066b9827694a4f5ad29c7e5514c55ccb1f
zhuzhengwei <zzw13655533995@163.com>
2020-12-03 19:19:34 +0800

    滑块验证登录captcha

--- app/src/main/java/com/alpha/meet/ui/TestActivity.java
+++ app/src/main/java/com/alpha/meet/ui/TestActivity.java
@@ -7,52 +7,37 @@ import android.widget.Toast;
 import androidx.annotation.Nullable;
 
 import com.alpha.framework.base.BaseUIActivity;
-import com.alpha.framework.view.PictureV;
 import com.alpha.meet.R;
+import com.luozm.captcha.Captcha;
 
 public class TestActivity extends BaseUIActivity {
-    private SeekBar mSeekBar;
-    private PictureV mPictureV;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.dialog_code_view);
-        initView();
-    }
 
-    private void initView() {
-        mSeekBar = (SeekBar) findViewById(R.id.mSeekBar);
-        mPictureV = (PictureV) findViewById(R.id.mPictureV);
-
-        mPictureV.setPictureVSucceed(new PictureV.OnPictureVSucceed() {
+        Captcha captcha = (Captcha) findViewById(R.id.captcha);
+        captcha.setCaptchaListener(new Captcha.CaptchaListener() {
             @Override
-            public void outValue(int pos, int maxValue) {
-                mSeekBar.setMax(maxValue);
-                mSeekBar.setProgress(pos);
+            public String onAccess(long time) {
+                Toast.makeText(TestActivity.this, "验证成功", Toast.LENGTH_SHORT).show();
+                return "验证通过,耗时" + time + "毫秒";
             }
 
             @Override
-            public void onSucceed() {
-                Toast.makeText(TestActivity.this, "成功", Toast.LENGTH_SHORT).show();
-            }
-        });
-
-        mSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
-            @Override
-            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
-                mPictureV.updateX(i);
+            public String onFailed(int failCount) {
+                Toast.makeText(TestActivity.this, "验证失败", Toast.LENGTH_SHORT).show();
+                return "验证失败,已失败" + failCount + "次";
             }
 
             @Override
-            public void onStartTrackingTouch(SeekBar seekBar) {
-
-            }
-
-            @Override
-            public void onStopTrackingTouch(SeekBar seekBar) {
-
+            public String onMaxFailed() {
+                Toast.makeText(TestActivity.this, "验证超过次数，你的帐号被封锁", Toast.LENGTH_SHORT).show();
+                return "验证失败,帐号已封锁";
             }
         });
     }
+
+
 }
